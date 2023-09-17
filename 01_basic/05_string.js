a0adcaaa0d9f8bef97730f36049ae85243e9d673
Shubhamyadav2003 <145142583+Shubhamyadav2003@users.noreply.github.com>
2023-09-17 15:12:33 +0000

    c

--- 01_basic/05_string.js
+++ 01_basic/05_string.js
@@ -1,5 +1,5 @@
 const name = "Shubham"
-const language = javascript
+const language = "javascript"
 // using STRING INTERPOLETION i.e
 
 console.log(`My name is ${name} and i am leraning ${language} language.`);
